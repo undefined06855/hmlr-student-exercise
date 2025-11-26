11ba9360bbaf307eaf2c394c530a0b4d7a845b05
undefined06855 <currentcashew57@gmail.com>
2025-11-26 11:57:11 +0000

    maybe i should test before i commit

--- app/entry/forms.py
+++ app/entry/forms.py
@@ -60,7 +60,7 @@ class EntryForm(FlaskForm):
     # A standard GOV.UK-styled submit button.
     submit: SubmitField = SubmitField("Save", widget=GovSubmitInput())
 
-    def __init__(self, register_id, entry_id = None, **kwargs):
+    def __init__(self, register_id, entry_id=None, **kwargs):
         super().__init__(**kwargs)
         self.register_id = register_id
         self.entry_id = entry_id
@@ -89,9 +89,15 @@ class EntryForm(FlaskForm):
         - Raising a ValidationError tells WTForms that this field is invalid,
           and the error message is displayed to the user.
         """
+
         existing = Entry.query.filter_by(register_id=self.register_id, name=field.data).first()
-        if existing and (self.entry_id and existing.id != self.entry_id):
-            raise ValidationError("Name already in use")
+
+        if self.entry_id == None:
+            if existing:
+                raise ValidationError("Name already in use")
+        else:
+            if existing and existing.id != self.entry_id:
+                raise ValidationError("Name already in use")
 
 
 class EntryDeleteForm(FlaskForm):
